be342309cd5edc8caa893954d6cdb645dd39afa8
GitHub Action <action@github.com>
2020-10-19 09:22:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/glennlefevere/stenciljswebcomponents/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/glennlefevere/stenciljswebcomponents/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.glennlefevere.stenciljswebcomponents.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.glennlefevere.stenciljswebcomponents.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/glennlefevere/stenciljswebcomponents/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.glennlefevere.stenciljswebcomponents.services
+
+import com.github.glennlefevere.stenciljswebcomponents.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
