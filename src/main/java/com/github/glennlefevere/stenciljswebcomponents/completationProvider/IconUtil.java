4ad7449dd416f27ead4460847e13e554a213960d
Glenn Lefevere <glenn.lefevere@liantis.be>
2021-01-06 11:11:42 +0100

    changed icons

--- src/main/java/com/github/glennlefevere/stenciljswebcomponents/completationProvider/IconUtil.java
+++ src/main/java/com/github/glennlefevere/stenciljswebcomponents/completationProvider/IconUtil.java
@@ -2,13 +2,15 @@ package com.github.glennlefevere.stenciljswebcomponents.completationProvider;
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 
+import com.intellij.util.ui.UIUtil;
 import javax.swing.*;
 import java.net.URL;
 
 public class IconUtil {
 
     public static LookupElementBuilder addIcon(LookupElementBuilder lookupElement) {
-        URL url = IconUtil.class.getClassLoader().getResource("preview_icon.png");
+        String dark = UIUtil.isUnderDarcula() ? "_dark" : "";
+        URL url = IconUtil.class.getClassLoader().getResource("preview_icon" + dark + ".png");
         if(url != null) {
             ImageIcon icon = new ImageIcon(url);
             return lookupElement.withIcon(icon);
